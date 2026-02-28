86e93b3924e48fae78e9e0ec7c87428a2620e2ac
Oleksii Tolstik <oleksii.tolstik@nure.ua>
2026-02-28 13:51:24 +0200

    Add nodemone for local testing

--- backend/stellheal-backend-app/server.js
+++ backend/stellheal-backend-app/server.js
@@ -85,7 +85,7 @@ async function main() {
     });
 
     app.listen(process.env.PORT || 4200, '0.0.0.0', () => {
-        console.log(`Server is running on port ${process.env.PORT || 4200}`);
+        console.log(`Server is running on port ${process.env.PORT || 4200} http://localhost:${process.env.PORT || 4200}`);
     });
 }
 

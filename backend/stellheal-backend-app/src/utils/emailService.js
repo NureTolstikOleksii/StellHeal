98c2fb33d321136f64d38efa5fbc961bfbe56449
Oleksii Tolstik <oleksii.tolstik@nure.ua>
2026-03-26 21:59:45 +0200

    fix back

--- backend/stellheal-backend-app/src/utils/emailService.js
+++ backend/stellheal-backend-app/src/utils/emailService.js
@@ -9,7 +9,7 @@ const transporter = nodemailer.createTransport({
     auth: {
         user: process.env.MAIL_USER,
         pass: process.env.MAIL_PASS,
-    },git
+    },
     tls: {
         rejectUnauthorized: false
     }

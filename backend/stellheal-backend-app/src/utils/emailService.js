095d4f500c209c0e52b7c34af531b1a8c49d50ae
Oleksii Tolstik <oleksii.tolstik@nure.ua>
2026-03-26 22:05:02 +0200

    fix back

--- backend/stellheal-backend-app/src/utils/emailService.js
+++ backend/stellheal-backend-app/src/utils/emailService.js
@@ -3,16 +3,11 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const transporter = nodemailer.createTransport({
-    host: 'smtp.gmail.com',
-    port: 465,
-    secure: true,
+    service: 'gmail',
     auth: {
         user: process.env.MAIL_USER,
         pass: process.env.MAIL_PASS,
     },
-    tls: {
-        rejectUnauthorized: false
-    }
 });
 
 export const sendWelcomeEmail = async (to, password) => {

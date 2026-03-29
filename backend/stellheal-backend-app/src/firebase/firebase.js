8c11cf984b0bbb62e1df79b748cf7ec2b18da438
Oleksii Tolstik <oleksii.tolstik@nure.ua>
2026-03-29 13:41:08 +0300

    fix firebase

--- backend/stellheal-backend-app/src/firebase/firebase.js
+++ backend/stellheal-backend-app/src/firebase/firebase.js
@@ -1,9 +1,6 @@
 import admin from 'firebase-admin';
-import fs from 'fs';
 
-const serviceAccount = JSON.parse(
-    fs.readFileSync(new URL('./serviceAccountKey.json', import.meta.url))
-);
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),

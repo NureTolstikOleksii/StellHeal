9cf0fc9b1c8ec9d6cc772967c1db2a89262a1b96
Oleksii Tolstik <oleksii.tolstik@nure.ua>
2026-05-28 16:42:21 +0300

    Update prisma connection

--- backend/stellheal-backend-app/src/config/prisma.js
+++ backend/stellheal-backend-app/src/config/prisma.js
@@ -1,11 +1,11 @@
 import { PrismaClient } from '@prisma/client';
 
-let prisma;
-
-if (!global.prisma) {
-    global.prisma = new PrismaClient();
-}
-
-prisma = global.prisma;
+const prisma = new PrismaClient({
+    datasources: {
+        db: {
+            url: process.env.DATABASE_URL,
+        },
+    },
+});
 
 export default prisma;
